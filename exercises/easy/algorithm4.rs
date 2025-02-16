df9f5dacd9c46eea157ff2044edd44e0c0a6df4f
dodola <flora_chen2021@163.com>
2025-02-16 21:33:02 +0800

    update:algo1,2,3

--- exercises/easy/algorithm4.rs
+++ exercises/easy/algorithm4.rs
@@ -1,12 +1,10 @@
 /*
-	binary_search tree
-	This problem requires you to implement a basic interface for a binary tree
+    binary_search tree
+    This problem requires you to implement a basic interface for a binary tree
 */
-
 
 use std::cmp::Ordering;
 use std::fmt::Debug;
-
 
 #[derive(Debug)]
 struct TreeNode<T>
@@ -43,7 +41,6 @@ impl<T> BinarySearchTree<T>
 where
     T: Ord,
 {
-
     fn new() -> Self {
         BinarySearchTree { root: None }
     }
@@ -51,12 +48,53 @@ where
     // Insert a value into the BST
     fn insert(&mut self, value: T) {
         //TODO
+        let mut root = self.root.as_mut();
+        while let Some(node) = root {
+            if value < node.value {
+                let mut left = node.left.as_mut();
+                match left {
+                    Some(ref mut left) => root = left.left.as_mut(),
+                    None => {
+                        left.insert(value);
+                        return;
+                    }
+                }
+            } else if value > node.value {
+                let mut right = node.right.as_mut();
+                match right {
+                    Some(ref mut right) => root = right.right.as_mut(),
+                    None => {
+                        right.insert(value);
+                        return;
+                    }
+                }
+            } else {
+                return;
+            }
+        }
     }
 
     // Search for a value in the BST
     fn search(&self, value: T) -> bool {
         //TODO
-        true
+        let mut root = self.root.as_ref();
+        while let Some(node) = root {
+            match node {
+                Some(ref node) => match value.cmp(&node.value) {
+                    Ordering::Less => {
+                        root = node.left.as_ref();
+                    }
+                    Ordering::Greater => {
+                        root = node.right.as_ref();
+                    }
+                    Ordering::Equal => {
+                        return true;
+                    }
+                },
+                None => return false,
+            }
+        }
+        false
     }
 }
 
@@ -67,9 +105,19 @@ where
     // Insert a node into the tree
     fn insert(&mut self, value: T) {
         //TODO
+        if value < self.value {
+            match self.left {
+                Some(ref mut left) => left.insert(value),
+                None => self.left = Some(Box::new(TreeNode::new(value))),
+            }
+        } else if value > self.value {
+            match self.right {
+                Some(ref mut right) => right.insert(value),
+                None => self.right = Some(Box::new(TreeNode::new(value))),
+            }
+        }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -79,24 +127,20 @@ mod tests {
     fn test_insert_and_search() {
         let mut bst = BinarySearchTree::new();
 
-        
         assert_eq!(bst.search(1), false);
 
-        
         bst.insert(5);
         bst.insert(3);
         bst.insert(7);
         bst.insert(2);
         bst.insert(4);
 
-        
         assert_eq!(bst.search(5), true);
         assert_eq!(bst.search(3), true);
         assert_eq!(bst.search(7), true);
         assert_eq!(bst.search(2), true);
         assert_eq!(bst.search(4), true);
 
-        
         assert_eq!(bst.search(1), false);
         assert_eq!(bst.search(6), false);
     }
@@ -105,22 +149,17 @@ mod tests {
     fn test_insert_duplicate() {
         let mut bst = BinarySearchTree::new();
 
-        
         bst.insert(1);
         bst.insert(1);
 
-        
         assert_eq!(bst.search(1), true);
 
-        
         match bst.root {
             Some(ref node) => {
                 assert!(node.left.is_none());
                 assert!(node.right.is_none());
-            },
+            }
             None => panic!("Root should not be None after insertion"),
         }
     }
-}    
-
-
+}
